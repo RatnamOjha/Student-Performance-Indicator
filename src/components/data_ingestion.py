4723ce5c8b04cee47fdfa67f41735b219ef30996
Ratnam Ojha <ratnamojha71@gmail.com>
2025-07-08 00:03:39 +0530

    new commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,17 @@
-import logging
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from numpy import e
 import pandas as pd
 import logging
 # from src.logger import logging
-import sys
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.exceptions import CustomException
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -23,8 +29,28 @@ class DataIngestion:
             # Load the dataset
             df = pd.read_csv('/Users/ratnamb.ojha/Downloads/StudentsPerformance.csv')
             logging.info("Read the dataset as dataframe")
-            
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data ingestion is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+                self.ingestion_config.raw_data_path
+            )
 
         
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
+        
