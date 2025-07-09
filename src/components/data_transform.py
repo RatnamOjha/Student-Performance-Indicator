5821f51c632ba845578850d0eba62f0bbba610ed
Ratnam Ojha <ratnamojha71@gmail.com>
2025-07-09 23:13:52 +0530

    new commit

--- src/components/data_transform.py
+++ src/components/data_transform.py
@@ -6,3 +6,45 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exceptions import CustomException 
+from src.logger import logging
+import os
+
+@dataclass
+class DataTransformConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransform:
+    def __init__(self):
+        self.data_transform_config = DataTransformConfig()
+
+    def get_data_transformer_object(self):
+        try: 
+            numerical_columns = ['writing score', 'reading score']
+            categorical_columns = [
+                'gender', 
+                'race/ethnicity', 
+                'parental level of education', 
+                'lunch', 
+                'test preparation course'
+            ]
+            num_pipeline = Pipeline(steps=[
+                ('imputer', SimpleImputer(strategy='median')),
+                ('scaler', StandardScaler())
+            ])
+            
+            cat_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='most_frequent')),
+                    ('onehotencoder', OneHotEncoder(handle_unknown='ignore'))
+                ]
+            )
+        
+        except:
+            pass
+
+
+
+
