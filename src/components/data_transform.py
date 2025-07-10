dc455a01c1677837aeba50255389f77755b6fa7c
Ratnam Ojha <ratnamojha71@gmail.com>
2025-07-10 19:08:37 +0530

    new commit

--- src/components/data_transform.py
+++ src/components/data_transform.py
@@ -20,7 +20,7 @@ class DataTransform:
     def __init__(self):
         self.data_transform_config = DataTransformConfig()
 
-    def get_data_transformer_object(self):
+    def get_data_transformer_object(self): #this is reponsible for data transformation
         try: 
             numerical_columns = ['writing score', 'reading score']
             categorical_columns = [
@@ -31,20 +31,49 @@ class DataTransform:
                 'test preparation course'
             ]
             num_pipeline = Pipeline(steps=[
-                ('imputer', SimpleImputer(strategy='median')),
-                ('scaler', StandardScaler())
+                ('imputer', SimpleImputer(strategy='median')), ##deals with missing values and replaces them with median
+                ('scaler', StandardScaler())   # standardize the data
             ])
             
             cat_pipeline = Pipeline(
                 steps=[
-                    ('imputer', SimpleImputer(strategy='most_frequent')),
-                    ('onehotencoder', OneHotEncoder(handle_unknown='ignore'))
+                    ('imputer', SimpleImputer(strategy='most_frequent')),  # deals with missing values and replaces them with mode
+                    ('onehotencoder', OneHotEncoder(handle_unknown='ignore')), # one-hot encoding
+                    ('scaler', StandardScaler())
                 ]
             )
+            logging.info(f'Numerical columns:{numerical_columns}')
+            logging.info(f'Categorical columns:{categorical_columns}')
+
+            preprocessor = ColumnTransformer(
+                [
+                ("num_pipeline", num_pipeline, numerical_columns),
+                ('cat_pipeline', cat_pipeline, categorical_columns)
+
+                ]
+            )
+            return preprocessor
         
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e, sys)
+            
+
+def initiate_data_transformation(self,train_path,test_path):
+    
+    try:
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
+
+        logging.info('Read train and test data completed')
+        logging.info('Obtaining Preprocessing object')
 
 
+        preprocessing_obj = self.get_data_transformer_object()
+
+        target_column_name = 'math score'
+        numerical_columns = ['writing score', 'reading score']
+
+    except:
+        pass
 
 
