9cf8689877e3b994d38b00bd3c7b10398913bbeb
Ratnam Ojha <ratnamojha71@gmail.com>
2025-07-13 22:55:59 +0530

    model training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,12 +1,52 @@
-class User:
-    pass     ## this lines does nothing
-user2 = User()
-user2.first_name = "Frank"
-user2.last_name = "Smith"
+import os
+import sys
+from dataclasses import dataclass
 
-print(user2.first_name, user2.last_name)
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
 
-user2.age = 37
-user2.is_active = True
+from src.exceptions import CustomException
+from src.logger import logging
 
-print(user2.first_name, user2.last_name, user2.age, user2.is_active)
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+
+        try:
+            logging.info("Splitting training and testing data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+            models ={
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors": KNeighborsRegressor(),
+                "AdaBoost": AdaBoostRegressor(),
+                "XGBClassifier": XGBRegressor(),
+            }
+
+            model_report = dict
+        
+        
+        except:
+            pass
