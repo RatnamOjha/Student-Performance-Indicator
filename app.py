4c6559d867a5996d46208be56fb28905ab26abcf
Ratnam Ojha <ratnamojha71@gmail.com>
2025-07-16 21:29:50 +0530

    restating app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template
 from src.pipeline.predict_pipleline import CustomData, PredictPipeline
 
 application = Flask(__name__)
